c8b516b7005f5d0a4a0b2ce3611ae098976ec894
Vít Novotný <witiko@mail.muni.cz>
2020-03-05 02:13:42 +0100

    Package BehroozMansouri/TangentCFT

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="tangentcft",
+    version="0.0.1",
+    description="Packaged version of BehroozMansouri/TangentCFT",
+    author="Vít Novotný",
+    author_email="witiko@mail.muni.cz",
+    url="https://github.com/Witiko/TangentCFT",
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=["setuptools"],
+    install_requires=["gensim~=3.8.1", "matplotlib~=3.0.3", "torch~=1.4.0"],
+    package_data={
+        "tangentcft": [
+            "Configuration/config/config_1",
+            "Retrieval_Results/*",
+            "TangentS/math_tan/87",
+            "TangentS/math_tan/mws.sty.ltxml",
+            "Visualization/formula_tsne",
+        ]
+    },
+)
